d3c52a548553ad3fe2adffe6b4cc1e27cb38913a
Prabhat-MSFT <ptrip@microsoft.com>
2017-02-01 02:53:36 +0000

    Add files via upload

---
+++ Model/LoanScoring.R
@@ -0,0 +1,71 @@
+#Remote Execution and Web Services for R Server
+library(mrsdeploy)
+help(mrsdeploy)
+
+# Read from csv file and create your model
+loans <- read.csv("C:/Users/azureuser/Documents/TechReady/LoanScoring/LoanStats.csv")
+head(loans, 5)
+
+# Train the model
+idx <- runif(nrow(loans)) > 0.75
+train <- loans[idx == FALSE,]
+
+loanDataModel <- rxDForest(is_bad ~ revol_util + loan_amnt + int_rate + mths_since_last_record + annual_inc_joint + dti_joint + total_rec_prncp + all_util, train)
+
+# Scoring Function
+loanPredictService <- function(revol_util, int_rate, loan_amnt, mths_since_last_record, annual_inc_joint, dti_joint, total_rec_prncp, all_util) {
+  inputData <- data.frame(revol_util = revol_util, loan_amnt = loan_amnt,int_rate = int_rate, mths_since_last_record = mths_since_last_record, annual_inc_joint = annual_inc_joint, dti_joint = dti_joint, total_rec_prncp = total_rec_prncp, all_util = all_util)
+  prediction <- rxPredict(loanDataModel, inputData)
+  loanScore <- prediction$is_bad_Pred 
+}
+remoteLogout()
+remoteLogin("http://40.86.94.151:12800", session = TRUE, diff = TRUE, commandline =  TRUE, 
+            prompt = "REMOTE_TECHREADY>>>", username = "admin", password = "Audi@2015")
+
+pause()
+
+# pseudo `unique` service name so no collision in example
+service_name <- paste0("loanPredictService", round(as.numeric(Sys.time()), 0))
+service_name <- "loanPredictService1484942117"
+
+# Publish service
+api <- publishService(
+  service_name,
+  code = loanPredictService,
+  model = loanDataModel,
+  inputs = list(revol_util = 'numeric', loan_amnt = 'numeric', int_rate = 'numeric', mths_since_last_record = 'numeric', annual_inc_joint = 'numeric', dti_joint = 'numeric', total_rec_prncp = 'numeric', all_util = 'numeric'),
+  outputs = list(loanScore = 'numeric'),
+  v = 'v1.0.0'
+)
+
+
+# Show API capabilities
+api$capabilities()
+
+#Consume the service
+loanScore <- api$loanPredictService(1, 1, 1, 1, 1, 1, 1,1)
+
+loanScore$output("loanScore")
+
+#List all services
+services <- listServices()
+services
+
+#Generate swagger json file
+cat(api$swagger(), file = "C:/Users/azureuser/Documents/TechReady/LoanScoring/loanPredict.json")
+
+cap <- api$capabilities()
+cap
+cap$swagger
+
+status <- deleteService(cap$name, cap$version)
+status
+
+services <- listServices(service_name)
+length(services) # gone
+
+# Error: No service found for `service_name` version "v1.0.0"
+api <- getService(service_name, "v2.0.0")
+
+#Logout
+remoteLogout()
